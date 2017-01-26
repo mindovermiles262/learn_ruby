e44e5328ee7f12b47b2da4e23601947b7de69d16
Erik Johnson <mindovermiles262@gmail.com>
2017-01-25 19:18:38 -0800

    passing pig latin translator

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,19 +1,35 @@
 #write your code here
-# TODO: Fix for two two words
-def translate (input)
-  words = input.split;
-  vow = ["a", "e", "i", "o", "u"];
-  words.each do |word|
-    # if first letter is vowel, add "ay" on end of word
-    if vow.include?(word[0])
-      output = word + "ay";
-    # else if first (and second) letters are consonants
-    elsif not vow.include?(word[0])
-      if not vow.include?(word[1])
-        output = word[2..-1] + word[0..1] + "ay";
-      else
-        output = word[1..-1] + word[0] + "ay";
-      end
+#PSEUDOCODE:
+#  for each word:
+#  find index of first vowel,
+#  continue rest of word
+#  add index[0] to index[first vowel] on end of string
+#  and "ay" at the end
+
+def translate_one (st)
+  vowels = ["a", "e", "i", "o", "u"];
+  i = 0; #index of first vowel
+  while i < st.length do
+    j = i + 2;
+    if st[i,2] == "qu"
+      i += 2;
+      break;
+    elsif not vowels.include?(st[i]);
+      i += 1;
+    else
+      break;
     end
   end
+  if i == 0
+    output = st + "ay";
+  else
+    output = st[i..-1] + st[0,i] + "ay";
+  end
+  return output;
 end
+
+def translate (st)
+  st.split.map { |word| translate_one(word) }.join(" ")
+end
+
+puts translate("quiet");
