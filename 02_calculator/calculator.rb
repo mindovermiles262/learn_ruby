1f0e9196551775feeffec4379d46d539bd408b4f
Erik Johnson <mindovermiles262@gmail.com>
2017-01-26 10:33:16 -0800

    finish 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,18 @@
 #write your code here
+def add (a, b)
+  output = a + b;
+  return output;
+end
+
+def subtract (a, b)
+  output = a - b;
+  return output;
+end
+
+def sum (numbers)
+  sum = 0;
+  numbers.each do |i|
+    sum += i;
+  end
+  return sum;
+end
