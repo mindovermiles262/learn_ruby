2f36ee404bd2bc3f27f50c6d2d4e9452bf0b4d6f
Erik Johnson <mindovermiles262@gmail.com>
2017-01-26 20:49:49 -0800

    finished lessons

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+
+  attr_reader :title;
+
+  def title=(st)
+    @title = format_title(st)
+  end
+
+  private
+
+  def format_title (st)
+    nono = ["the", "a", "an", "and", "in", "of"]
+    words = st.split
+    words.map do |x|
+      x.capitalize! unless nono.include?(x);
+    end
+    words[0].capitalize!
+    words.join(" ");
+  end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
 class Timer
   #write your code here
+  attr_reader :hours, :minutes, :seconds;
+
+  def initialize
+    @hours = 00;
+    @minutes = 00;
+    @seconds = 00;
+  end
+
+  def time_string
+    @time_string = "%02d" % @hours + ":" + "%02d" % @minutes + ":" + "%02d" % @seconds
+  end
+
+  def seconds=(sec)
+    hr = sec/3600;
+    sec -= (hr * 3600);
+    min = sec/60;
+    sec -= (min * 60);
+    @hours += hr;
+    @minutes += min;
+    @seconds += sec;
+  end
 end
